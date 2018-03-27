1075f42161a3505776a5de30fd7fd016240584c3
Juan E Afable <juanjuanzerojuan@gmail.com>
2018-03-26 22:09:15 -0400

    Made changes to the database table checker.

--- HowMuchWater.py
+++ HowMuchWater.py
@@ -6,7 +6,8 @@
 # The goal of this project is to download data from an API, parse the data, load it into a local database and create an analytical reports for this data. For this exercise we will be pulling data from the United States Geological Survey (USGS) water service to determine how much *streamflow* was recorded at the Ohio River by the flow meter at the Louisville Water Station (03292494). The user can enter dates of their choosing if they like.
 # 
 # **Version**
-# <p>This tool was made with Python 3.6.5 and Jupyter Notebooks 5.0.0</p>
+# <p>This tool was made with Python 3.6.1 and Jupyter Notebooks 5.0.0</p>
+# <p>A requirements.txt accompanies this file, which defines all of the requirements used (including others) in developing this tool. Be sure to install the requests and bokeh packages into your environment before running the program.</p>
 # 
 # **Tools**
 # * *USGS Data* :https://waterservices.usgs.gov/rest/DV-Service.html
@@ -20,16 +21,17 @@
 # I just wanted to say a very big thanks to all of mentors at Code Louisville who guided, taught and helped me get to this point!
 # 
 
-# In[2]:
+# In[24]:
 
 #import pandas as pd
-#import os
+import os
+import csv
 import requests
 import json
-from datetime import datetime,date
+from datetime import datetime, date, timedelta
 from collections import namedtuple
 import sqlite3
-from bokeh.io import output_file, show
+from bokeh.io import output_file, show, output_notebook
 from bokeh.plotting import figure
 from bokeh.models import HoverTool, NumeralTickFormatter, ColumnDataSource
 
@@ -38,7 +40,7 @@ from bokeh.models import HoverTool, NumeralTickFormatter, ColumnDataSource
 # 
 # So we need to get the date parameters to construct it into a string for our request to the API. We create a function that asks the user to make the request. I also added in some validation so that its in the proper format.
 
-# In[3]:
+# In[25]:
 
 site = '03292494'
 tbl_name = 'site_'+site
@@ -98,7 +100,7 @@ while(not out):
 
 # <h2>Also, check if the response of the request</h2>
 
-# In[4]:
+# In[26]:
 
 test_url = requests.get(url)
 if test_url.status_code != 200:
@@ -111,7 +113,7 @@ else:
 # 
 # Here we use the json library to get the text response and access the values. I printed them out here so we can check if we are getting the correct response.
 
-# In[5]:
+# In[27]:
 
 response_url = requests.get(url)
 parsed_json = json.loads(response_url.text)
@@ -125,7 +127,7 @@ print(parsed_json['value']['timeSeries'][0]['values'][0]['value'][3])
 #print(parsed_json)
 
 
-# In[6]:
+# In[28]:
 
 #save the list of values in a variable. (because...)
 daily_dis = parsed_json['value']['timeSeries'][0]['values'][0]['value']
@@ -152,7 +154,7 @@ def insp_Request(request_list):
 # 
 # So now that we have our data where we want it we can now load it into a SQL database. But before we do that we should use the namedtuple parameter in case we had to handle a larger table to load into sql
 
-# In[7]:
+# In[29]:
 
 daily_discharge = namedtuple('daily_discharge','date, discharge, qualifier')
 discharge_list = []
@@ -172,12 +174,13 @@ for val in daily_dis:
     discharge_list.append(daily_discharge(date_text,discharge,qualifier))
     
 #check how you can access the namedtuple
-print(discharge_list[0].discharge)
-print(discharge_list[0].date)
-print(discharge_list[0].qualifier)
+#print(discharge_list[0].discharge)
+#print(discharge_list[0].date)
+#print(discharge_list[0].qualifier)
+print(discharge_list[0])
 
 
-# In[8]:
+# In[9]:
 
 #checking the range of what we are about to input into the database.
 print(discharge_list[0].date)
@@ -194,31 +197,52 @@ print(discharge_list[len(discharge_list)-1].date)
 # 
 # When i was thinking about the design of this i wanted the user to be able to run this script again and load as much data they wanted *or needed* in the database for reporting. So the next step goes through checking if the table already exists and then later checking if the record for that table already exists to prevent duplication.
 
-# In[9]:
+# In[30]:
 
 conn = sqlite3.connect('daily_discharge.db')
 cur = conn.cursor()
 
 
+# In[31]:
+
 sql_text = (tbl_name,)
+#print(sql_text)
 #check if the table exists, TRUE if the select statement returns nothing, FALSE if it returns something.  
-Not_exists = cur.execute('SELECT name FROM sqlite_master WHERE type="table" AND name=?',sql_text) is None
+List_tables = cur.execute('SELECT name FROM sqlite_master WHERE type="table" AND name=?',sql_text)
+
+Not_exists = True
+
+#loop through the returned table and output check if it exists
+for i in List_tables:
+    #print(i)
+    if i[0] == tbl_name:
+        print("The table already exists") #found the matching table and breaks it.
+        Not_exists = False
+        break
+        
+
+
+# In[32]:
 
 #if the table exists do nothing
 if(Not_exists):
-    print('it does not exist')
+    print(f'The table, {tbl_name} does not exist yet\n Creating {tbl_name}')
     #create the table if it does not exist
-    cur.execute('''CREATE TABLE ?(date TEXT, discharge REAL, qualifier TEXT)''',sql_text)
-    cur.commit()
+    try:
+        query = f'CREATE TABLE {sql_text[0]}(date TEXT, discharge REAL, qualifier TEXT)'
+        cur.execute(query)
+        conn.commit()
+    except:
+        print("This table may already exist.")
 else:
-    print('The table {} already exist'.format(sql_text[0]))
+    print('The table {} already exists'.format(sql_text[0]))
     #insert the values of the table into the created table
 
 
 # <h2>Preventing Duplicate Records</h2>
 # This is a time series dataset for each day of the station and there could a point where we request data that is already in the dataset. So lets run a few sql commands to the existing database to see if the data is already there. If it does, then we just ignore it (#needResearch). If not, then we insert that record.
 
-# In[10]:
+# In[33]:
 
 for record in discharge_list:
     date = (tbl_name, record.date)
@@ -242,7 +266,7 @@ for record in discharge_list:
 # <h2>Getting Data Back for Reports</h2>
 # Great! Now we have data that can persist through multiple uses! Woop Woop! Next is we get all of the records that we have to see the trend of waterflow that went pass the sensor at the Louisville Water Tower station.
 
-# In[11]:
+# In[34]:
 
 results = cur.execute('SELECT * FROM site_03292494 ORDER BY date DESC')
 
@@ -250,25 +274,41 @@ dates = []
 discharges = []
 for row in results:
     #print('Date: {} \tDischarge: {} \t Qualifier:{}'.format(row[0],row[1],row[2]))
-    date_text = row[0]
-    date = datetime.strptime(date_text, '%Y-%m-%d').date()
-    dates.append(date)
-    discharges.append(row[1])
+    if row[1] > 0:
+        discharges.append(row[1])
+        date_text = row[0]
+        date = datetime.strptime(date_text, '%Y-%m-%d').date()
+        dates.append(date)
+    else:
+        continue
+    
+    
+    
 
 
 
 # <h2>Using Bokeh for Plots</h2>
 # In order to see that data we just pulled I used Bokeh to plot it out.
 
-# In[13]:
+# In[35]:
 
-output_file("discharges.html")
+output_notebook()
 
-TOOLs = "pan,lasso_select,box_select,tap"
+
+# <h1>How Much Water?</h1>
+# 
+# As you can see that the peak flow of the request is done. The user can keep making the same requests for a larger and larger database. If the user looked for data for 2018-01-01 to 2018-03-01 then the user would have seen that *655,000 cubic feet/second* of water flowed through the Ohio River Station at the Louisville Water Tower. Thats a lot!
+
+# In[36]:
+
+#output_file("discharges.html")
+
+TOOLs = "pan,lasso_select,box_select,tap,zoom_in,zoom_out"
 
 source = ColumnDataSource(data=dict(dates=dates,discharges=discharges))
 
 plot_fig = figure(plot_width=700, plot_height=500, title="Ohio River Station 03292494", x_axis_type='datetime', tools=TOOLs)
+
 plot_fig.circle('dates', 'discharges', size=8, color='navy', alpha=0.5, source=source)
 plot_fig.line('dates','discharges', color='navy', source=source)
 
@@ -284,11 +324,10 @@ plot_fig.add_tools(hover)
 plot_fig.yaxis.formatter = NumeralTickFormatter(format='0,0')
 plot_fig.yaxis.axis_label = "Streamflow (cfs)"
 
-
 show(plot_fig)
 
 
-# In[14]:
+# In[23]:
 
 #disconnect to the sql database
 conn.close()
@@ -296,10 +335,23 @@ conn.close()
 
 # what if it was stored in a csv? the data is stored as a text file in the data folder. Now based on the file structure there is a data folder where the root of the 
 # 
+# **More research**
+# look up 
+# *click libraries
+# *goey libraries
+# 
+# https://pypi.python.org/pypi/Gooey
+
 # ```
 # #get the current working directory
-# #path = os.getcwd()
+# path = os.getcwd()
+# file = 'MSDRainGaugeReport25.csv'
+# folder = '\\data\\'
+# full_file = path+folder+file
 # 
-# #data = pd.read_csv(path+'/'+'data/BeargrassCreek.txt', sep='\t', header=1)
-# #print(data)
+# with open(full_file, 'rt') as csvfile:
+#     read_csv_data = csv.reader(csvfile)
+#     #print(read_csv_data)
+#     for row in read_csv_data:
+#         print(row)
 # ```
